bc21a997ee2adaf1e44ccbd7506d413c833adcf2
JoshuaBillson <jmbillson@outlook.com>
2024-07-19 21:09:29 -0600

    ResNet

--- src/models/resnet.jl
+++ src/models/resnet.jl
@@ -7,16 +7,18 @@ end
 Flux.@layer ResNet
 
 function ResNet(depth::Int; pretrain=false, channels=3, nclasses=1000)
-    base = Metalhead.ResNet(depth, pretrain=false, inchannels=channels, nclasses=nclasses)
     if pretrain
         pretrained = Metalhead.ResNet(depth, pretrain=true, inchannels=3, nclasses=nclasses)
-        input = Metalhead.backbone(base)[1]
-        backbone = Metalhead.backbone(pretrained)[2:end]
+        p_backbone = Metalhead.backbone(pretrained)
+        input = Flux.Chain(Flux.Conv((7, 7), channels => 64, pad=3, stride=2, bias=false), Flux.BatchNorm(64, Flux.relu))
+        backbone = Flux.Chain(Flux.Chain(Flux.MaxPool((3, 3), pad=1, stride=2), p_backbone[2]...), p_backbone[3:end]...)
         head = Metalhead.classifier(pretrained)
         return ResNet(input, backbone, head)
     else
-        input = Metalhead.backbone(base)[1]
-        backbone = Metalhead.backbone(base)[2:end]
+        base = Metalhead.ResNet(depth, pretrain=false, inchannels=channels, nclasses=nclasses)
+        p_backbone = Metalhead.backbone(base)
+        input = Metalhead.backbone(base)[1][1:2]
+        backbone = Flux.Chain(Flux.Chain(Flux.MaxPool((3, 3), pad=1, stride=2), p_backbone[2]...), p_backbone[3:end]...)
         head = Metalhead.classifier(base)
         return ResNet(input, backbone, head)
     end
@@ -26,7 +28,7 @@ function activations(m::ResNet, x)
     input_out = m.input(x)
     backbone_out = Flux.activations(m.backbone, input_out)
     head_out = last(backbone_out) |> m.head
-    return [input_out, backbone_out..., head_out]
+    return Tuple([input_out, backbone_out..., head_out])
 end
 
 function (m::ResNet)(x)
