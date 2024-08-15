439e74899a15bd1ad5d6db12b833a3fcd5bb2230
JoshuaBillson <jmbillson@outlook.com>
2024-08-15 17:57:40 -0600

    Modified test cases to use StableRNGs

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using Apollo
 using Test
 using Rasters
 using Random
+using StableRNGs
 
 @testset "transforms" begin
     # Test Data
@@ -61,20 +62,20 @@ end
     @test all(repeatobs(zipobs(v1, v2), 2) .== reduce(vcat, [collect(zip(x1, x2)) for _ in 1:2]))  # zipobs + repeatobs
 
     # splitobs with shuffle
-    @test first(splitobs(Random.MersenneTwister(123), 1:10, at=0.7)) == [8, 7, 5, 3, 4, 1, 6]
-    @test last(splitobs(Random.MersenneTwister(123), 1:10, at=0.7)) == [10, 2, 9]
-    @test splitobs(Random.MersenneTwister(123), 1:10, at=[0.3, 0.5])[1] == [8, 7, 5]
-    @test splitobs(Random.MersenneTwister(123), 1:10, at=[0.3, 0.5])[2] == [3, 4, 1, 6, 10]
-    @test splitobs(Random.MersenneTwister(123), 1:10, at=[0.3, 0.5])[3] == [2, 9]
+    @test first(splitobs(StableRNGs.StableRNG(123), 1:10, at=0.7)) == [4, 7, 2, 1, 3, 8, 5]
+    @test last(splitobs(StableRNGs.StableRNG(123), 1:10, at=0.7)) == [6, 10, 9]
+    @test splitobs(StableRNGs.StableRNG(123), 1:10, at=[0.3, 0.5])[1] == [4, 7, 2]
+    @test splitobs(StableRNGs.StableRNG(123), 1:10, at=[0.3, 0.5])[2] == [1, 3, 8, 5, 6]
+    @test splitobs(StableRNGs.StableRNG(123), 1:10, at=[0.3, 0.5])[3] == [10, 9]
     @test_throws ArgumentError splitobs(1:10, at=[0.3, 0.8])
     @test map(length, splitobs(1:10, at=[0.3, 0.7])) == [3, 7, 0]
 
     # splitobs without shuffle
-    @test first(splitobs(Random.MersenneTwister(123), 1:10, at=0.7; shuffle=false)) == [1, 2, 3, 4, 5, 6, 7]
-    @test last(splitobs(Random.MersenneTwister(123), 1:10, at=0.7; shuffle=false)) == [8, 9, 10]
-    @test splitobs(Random.MersenneTwister(123), 1:10, at=[0.3, 0.5]; shuffle=false)[1] == [1, 2, 3]
-    @test splitobs(Random.MersenneTwister(123), 1:10, at=[0.3, 0.5]; shuffle=false)[2] == [4, 5, 6, 7, 8]
-    @test splitobs(Random.MersenneTwister(123), 1:10, at=[0.3, 0.5]; shuffle=false)[3] == [9, 10]
+    @test first(splitobs(StableRNGs.StableRNG(123), 1:10, at=0.7; shuffle=false)) == [1, 2, 3, 4, 5, 6, 7]
+    @test last(splitobs(StableRNGs.StableRNG(123), 1:10, at=0.7; shuffle=false)) == [8, 9, 10]
+    @test splitobs(StableRNGs.StableRNG(123), 1:10, at=[0.3, 0.5]; shuffle=false)[1] == [1, 2, 3]
+    @test splitobs(StableRNGs.StableRNG(123), 1:10, at=[0.3, 0.5]; shuffle=false)[2] == [4, 5, 6, 7, 8]
+    @test splitobs(StableRNGs.StableRNG(123), 1:10, at=[0.3, 0.5]; shuffle=false)[3] == [9, 10]
 
     # takeobs
     @test all(takeobs(v1, [2, 5, 8, 9]) .== x1[[2, 5, 8, 9]])  # takeobs
@@ -91,13 +92,13 @@ end
     @test all(mapobs(x -> x * 2 + 1, v1) .== [3, 5, 7, 9, 11, 13, 15, 17, 19, 21])
 
     # sampleobs
-    @test all(sampleobs(Random.MersenneTwister(126), v2, 4) .== [22, 28, 25, 30])
+    @test all(sampleobs(StableRNGs.StableRNG(126), v2, 4) .== [25, 24, 30, 26])
     @test length(sampleobs(v1, 0)) == 0
     @test_throws ArgumentError sampleobs(v2, -1)
     @test_throws ArgumentError sampleobs(v2, length(v2) + 1)
 
     # shuffleobs
-    @test all(shuffleobs(Random.MersenneTwister(123), v1) .== [8,7,5,3,4,1,6,10,2,9])
+    @test all(shuffleobs(StableRNGs.StableRNG(123), v1) .== [4, 7, 2, 1, 3, 8, 5, 6, 10, 9])
 
     # TileView
     tile = Raster(rand(UInt16, 256, 256, 2, 8), (X, Y, Band, Ti))
